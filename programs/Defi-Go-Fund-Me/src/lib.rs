4b41e88d4d8c73a8318fc0519be29f825fd52fd5
Devon <dmmartens91@gmail.com>
2022-09-21 21:17:54 -0400

    removes boilerplate

--- programs/Defi-Go-Fund-Me/src/lib.rs
+++ programs/Defi-Go-Fund-Me/src/lib.rs
@@ -6,10 +6,6 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod defi_go_fund_me {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
-    }
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
+
