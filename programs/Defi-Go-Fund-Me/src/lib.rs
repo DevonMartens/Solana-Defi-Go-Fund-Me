c299937f2996297939114ed4f69aa4e284a11da5
Devon <dmmartens91@gmail.com>
2022-09-21 21:37:18 -0400

    creating campaign account

--- programs/Defi-Go-Fund-Me/src/lib.rs
+++ programs/Defi-Go-Fund-Me/src/lib.rs
@@ -10,7 +10,8 @@ pub mod defi_go_fund_me {
     /*
     function to create campagign
     @Dev: only owner of campagin can withdraw
-    @Notice: in anchor context is always the first argument to get data from
+    @Notice: In anchor context is always the first argument to get data from.
+    @Notice: The context of the function in Solana is the list of accounts that that function needs to retrieve data from.
     @Params: 1. Name - string allows users to state campagin name 2. Desc - desc of campagn
     @Dev: specify return value as a result (returns fn results and errors) */
     pub fn create(ctx: Context<Create>, name: String, description: String) -> Result<()> {
@@ -28,6 +29,21 @@ pub mod defi_go_fund_me {
 
     }
 
+}
+/*
+Macro explains that this is a context
+*/
+#[derive(Accounts)]
+pub struct Create<'info>{
+    //create a campaign account with this fn - why there is an init macro
+    #[account(init, payer=user, space=9000)]
+    pub campaign: Account<'info, Campaign>,
+    //speficies this as mutable so it can be changed
+    #[account(mut)]
+    pub user: Signer<'info>,
+    //system specifications of blockchain
+    //we dont use init here or with user b/c we dont create either
+    pub system_program: Program<'info, System>
 }
 
 
