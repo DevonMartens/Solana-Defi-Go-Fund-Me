ac6d50392c112ac92a7041febd52b0b55127f81c
Devon <dmmartens91@gmail.com>
2022-09-22 11:50:28 -0400

    wallet approves txn

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,33 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
+import idl from './idl.json';
+import { Connection, PublicKey, clusterApiUrl } from '@solana/web3.js';
+import { Program, AnchorProvider, web3, utils, BN } from '@project-serum/anchor';
+import { Buffer } from 'buffer';
+window.Buffer = Buffer;
+//variables to make connections to the contract
 
+//give app program id
+const programId = new PublicKey(idl.metadata.address);
+//specify network
+const network = clusterApiUrl("devnet");
+//how to identify a complete txn
+const opts = {
+  preflightCommitment: "processed",
+  //this is how you choose "when" to recieve a confirmation that a transaction has succeeded
+}
+//
+const { SystemProgram } = web3;
 
+//app code
 const App = () => {
 const [walletAdd, setWalletAddress ] = useState(null);
+//function to get provider - connect to solana
+const getProvider = () => {
+  const connection = new Connection(network, opts.preflightCommitment);
+  const provider = new AnchorProvider(connection, window.solana, opts.preflightCommitment);
+  return provider;
+}
 const CheckConnection = async() => {
   //stateful variable to hold wallet as a constant
   try {
@@ -44,11 +68,48 @@ const CheckConnection = async() => {
       setWalletAddress(response.publicKey)
     }
   };
+//function to create campaign
+const createCampaign = async () => {
+  try{
+    //get provider + program
+    const provider = getProvider()
+    const program = new Program(idl, programId, provider)
+    //get address created with capaign acct
+    const [campaign] = await PublicKey.findProgramAddress([
+      utils.bytes.utf8.encode("Campaign_demo"),
+      provider.wallet.publicKey.toBuffer(),
+    ],
+      programId
+    );
+    //address calc now time to create account
+    //need to make txt box for name and descr
+    await program.rpc.create("campaign name", "campaign descr", {
+      accounts: {
+        campaign,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+    },
+  });
+  console.log("new camapign = success at address:", campaign.toString)
+  } catch(error){
+    console.log("Error: Campaign create failed:", error);
+  }
+}
+
+
+
+
+
   //component to connect wallet if wallet is not present
   const ConnectedWallet = () => (
     //render button
     <button onClick={connectWallet}>Connect to this dApp</button>
   );
+  //component to create camapgin if wallet is not present
+  const createButton = () => (
+      //render button
+      <button onClick={createCampaign}>Create Campaign</button>
+    );
   //call to check the if wallet is connected function
   //use effect hook gets called on componenet mounts
   //second argument i.e. this array is empty.
@@ -69,6 +130,7 @@ const CheckConnection = async() => {
   return (
   <div className='App'>
     {!walletAdd && ConnectedWallet()}
+    {walletAdd && createButton()}
     </div>
   );
 };
