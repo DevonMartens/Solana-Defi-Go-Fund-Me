c61afe1ed1d8d9c2557cb618f97fb8ba83ac2989
Devon <dmmartens91@gmail.com>
2022-09-22 09:50:52 -0400

    front end detects phantom wallet

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App =() => {
+const CheckConnection = async() => {
+  try {
+    //solana object automatically injected via phantom
+    const { solana } = window;
+    //check if object is there
+    if (solana) {
+    if (solana.isPhantom) {
+      console.log("The phantom is present");
+    }
+   } else {
+      alert("download wallet here: https://phantom.app/download");
+    }
+  } catch(error) {
+    console.error(error);
+  }
+  };
+  //call to check the if wallet is connected function
+  //use effect hook gets called on componenet mounts
+  //second argument i.e. this array is empty.
+  // that code will be called when the page floats.
+  useEffect(() => {
+    //check if we have connection
+    const onLoad = async() => {
+     await CheckConnection();
+    };
+    //the add onload as an event listener to the window
+    window.addEventListener("load", onLoad);
+    //return a remove event listener when window closed
+    return() => window.removeEventListener("load", onLoad);
+    //arg 2 an array - "this array"
+  }, []);
+};
 
 export default App;
